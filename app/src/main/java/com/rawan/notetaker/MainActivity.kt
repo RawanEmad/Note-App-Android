baf8baf0e9222139318fdfef297fe686d808871a
unknown <rawan.emadnegm@gmail.com>
2023-01-09 00:42:11 +0200

    Supporting Email Account Creation

--- app/src/main/java/com/rawan/notetaker/MainActivity.kt
+++ app/src/main/java/com/rawan/notetaker/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.rawan.notetaker
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import com.rawan.notetaker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val TAG = MainActivity::class.qualifiedName
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,14 +22,46 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        binding.btnSignIn.setOnClickListener {}
+        binding.btnSignIn.setOnClickListener {
 
-        val intent = Intent(this, ListActivity::class.java)
-        intent.putExtra(USER_ID, "-1")
-        startActivity(intent)
+            val providers = arrayListOf(
+                AuthUI.IdpConfig.EmailBuilder().build()
+            )
+
+            startActivityForResult(AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(), RC_SIGN_IN)
+        }
+
+        val auth = FirebaseAuth.getInstance()
+
+        if (auth.currentUser != null) {
+            val intent = Intent(this, ListActivity::class.java)
+            intent.putExtra(USER_ID, auth.currentUser!!.uid)
+            startActivity(intent)
+        }
     }
 
     companion object {
         const val USER_ID = "user_id"
+        const val RC_SIGN_IN = 15
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN){
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK){
+                val user = FirebaseAuth.getInstance().currentUser
+                val intent = Intent(this, ListActivity::class.java)
+                intent.putExtra(USER_ID, user!!.uid)
+                startActivity(intent)
+            } else {
+                Log.e(TAG, "Sign-in failed ", response!!.error)
+            }
+        }
     }
 }
